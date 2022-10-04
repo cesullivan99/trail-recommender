662ccf467e5a1a2a7554e3df4b684fd5d9589d2c
cesullivan99 <cesullivan99@gmail.com>
2022-10-04 21:55:18 +1100

    Small changes to header

--- trail.py
+++ trail.py
@@ -1,8 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+# Created By  : Carter Sullivan
+# Created Date: 10/4/2022
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""This module built to deal with the trail class, and provide related functionality"""
+# ---------------------------------------------------------------------------
 
 
-# to classify a trail - this is basic, may add more later
+# to classify a trail - this is basic, will likely add more later
 class Trail:
     def __init__(self, name, difficulty, rating, distance, descent, climb):
         self.name = name
