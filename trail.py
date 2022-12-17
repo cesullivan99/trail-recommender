cea51b856d1dc9c0ec0dabfc03eb0f6e680c2166
cesullivan99 <cesullivan99@gmail.com>
2022-12-17 14:20:30 -0500

    Small changes to header

--- trail.py
+++ trail.py
@@ -1,17 +1,39 @@
 # Created By  : Carter Sullivan
 # Created Date: 10/4/2022
 # version ='1.0'
-# ---------------------------------------------------------------------------
-"""This module built to deal with the trail class, and provide related functionality"""
-# ---------------------------------------------------------------------------
+"""This module contains the Trail class, as well as relevant methods"""
 
 
-# to classify a trail - this is basic, will likely add more later
 class Trail:
-    def __init__(self, name, difficulty, rating, distance, descent, climb):
+    """
+    A class representing a trail
+
+    Attributes:
+        name (str): The name of the trail
+        difficulty (int): the difficulty of the trail
+        distance (int): the length of the trail
+        descent (int): the length of the trail's descent
+        climb (int): the length of the trail's climb
+    Methods:
+        __init__(name: str, difficulty: int, distance: int, descent: int)
+            Constructs a trail object
+    """
+
+    def __init__(self, name, difficulty, distance, descent, climb):
+        """
+        The function takes in the name, difficulty, distance, descent, and climb of a trail and assigns them to the
+        corresponding attributes of the trail.
+
+        :param name: the name of the trail
+        :param difficulty: a number from 1 to 5
+        :param distance: the distance of the trail in miles
+        :param descent: the total elevation descent in meters
+        :param climb: the total elevation gain in meters
+        """
+
         self.name = name
         self.difficulty = difficulty
-        self.rating = rating
         self.distance = distance
-        self.descent = descent
-        self.climb = climb
+        # descent/climb ratio
+        self.dc = descent / climb
+
